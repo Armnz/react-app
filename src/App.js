ff9230eb1c8152946eae8121c9700758b117c909
Armand <armands.elsbergs@gmail.com>
2024-01-07 23:50:02 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Title from "./components/Title";
+import Modal from "./components/Modal";
+import EventList from "./components/EventList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [showModal, setShowModal] = useState(false);
+	const [showEvents, setShowEvents] = useState(true);
+	const [events, setEvents] = useState([
+		{ title: "Mario's birthday bash", id: 1 },
+		{ title: "Bowser's live stream", id: 2 },
+		{ title: "race on moo moo farm", id: 3 },
+	]);
+
+	console.log(showModal);
+
+	const handleClick = (id) => {
+		setEvents((prevEvents) => {
+			return prevEvents.filter((event) => {
+				return id !== event.id;
+			});
+		});
+	};
+
+	const handleClose = () => {
+		setShowModal(false);
+	};
+
+	const subtitle = "All the latest events in Mario Kingdom";
+
+	return (
+		<div className="App">
+			<Title title="Events in your area" subtitle={subtitle} />
+			{showEvents && (
+				<div>
+					<button onClick={() => setShowEvents(false)}>hide events</button>
+				</div>
+			)}
+			{!showEvents && (
+				<div>
+					<button onClick={() => setShowEvents(true)}>show events</button>
+				</div>
+			)}
+			{showEvents && <EventList events={events} handleClick={handleClick}/>}
+			{/* 			<Modal>
+				<h2>10% Off Coupon Code!!</h2>
+				<p>Use the code NINJA10 at the checkout.</p>
+			</Modal> */}
+			{showModal && (
+				<Modal handleClose={handleClose}>
+					<h2>Terms and Conditions</h2>
+					<p>
+						Nisi elit fugiat non esse amet in officia amet Lorem sit quis et id
+						ex. Pariatur deserunt ut sunt sunt ex. Magna magna excepteur eiusmod
+						eiusmod deserunt ipsum qui mollit ad. Et nostrud sint ad ullamco
+						cupidatat culpa dolore est dolor culpa reprehenderit commodo aliquip
+						incididunt.
+					</p>
+				</Modal>
+			)}
+			<div>
+				<button onClick={() => setShowModal(true)}>
+					Read Terms and Conditions
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
